b8717119a67d84659af85e0e38f78f7584c2d019
KaDiasz <00001133166404sp@al.educacao.sp.gov.br>
2024-10-24 16:00:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'Como funciona o curso comandos?',
+    ' Durante esse período, a maioria das atividades é realizada de forma contínua com ênfase nas técnicas e táticas empregadas em operações especiais específicas de Comandos'
+)
+
+criaCartao(
+    'Educação financeira',
+    'quando surgiu a educação financeira no brasil?',
+    'No Brasil, a educação financeira vem conquistando espaço como política de Estado a partir da publicação do Decreto nº 7.397, de 22 dezembro de 2010, que instituiu a Estratégia Nacional de Educação Financeira (Enef)'
+)
+
+criaCartao(
+    'Lingua Portugesa',
+    'Qual a importância do livro Senhora?',
+    'Através dos diálogos e discussões entre Fernando e Aurélia, podemos notar a visão crítica que estes possuem da sociedade, onde o casamento não é apenas por amor, e mais por interesse'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Eu descobrir um novo talento em Inglês?',
+    'Eu descobrir um novo talento em ingles é I discover a new talent'
+)
+
+criaCartao(
+    'biotecnologia',
+    'Qual a diferença entre o RNA e o DNA?',
+    'DNA e RNA são ácidos nucléicos que possuem diferentes estruturas, mas que apresentam funções indispensáveis para o metabolismo e a reprodução de um organismo. Enquanto o DNA é responsável por armazenar as informações genéticas dos seres vivos, o RNA atua na produção de proteínas'
+)
+
+criaCartao(
+    'Educacão fisica',
+    'O que dertemina a individualidade biologica?',
+    'a individualidade biologica reage de forma individual ao exercicios executados variando conforme o sexo'
+)
+
+criaCartao(
+    'Matemática',
+    'O que probabilidade?',
+    'E o matemática que estuda as chances de determinado evento aconteçer e os resultados de esperimentos aleatorios'
+)
